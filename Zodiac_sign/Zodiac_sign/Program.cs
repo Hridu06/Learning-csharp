10dbbd27b62276d6f489987d4181a2ea18fafe13
Hridu06 <sabbirhossainhridoy06@gmail.com>
2025-08-13 23:40:30 +0600

    initial commit

---
+++ Zodiac_sign/Zodiac_sign/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ChineseZodiac
+{
+    static void Main()
+    {
+        string[] zodiac = {
+            "Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox",
+            "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep"
+        };
+
+        Console.Write("Enter your birth year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        int index = year % 12;
+        Console.WriteLine("Your Chinese Zodiac is: " + zodiac[index]);
+    }
+}
