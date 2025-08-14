abca953344889b38d6125f3e0ce5ea962952e0da
Hridu06 <sabbirhossainhridoy06@gmail.com>
2025-08-14 23:48:59 +0600

    initial comment

---
+++ Perfect_number/Perfect_number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the start range: ");
+            int start = int .Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the end range: ");
+            int End = int .Parse(Console.ReadLine());
+
+            Console.WriteLine("Perfect numbers in this range:");
+            for (int i = start; i <= End; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j <= i/2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                        
+                }
+
+                if (i == sum && i != 0)
+                {
+                    Console.WriteLine(i);
+                }
+                    
+            }
+        }
+    }
+}
